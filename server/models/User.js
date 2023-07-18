83eccc1596c15c116ba5d2cb88708f649a0a34b1
warengeorge <georgeomiebi@gmail.com>
2023-07-18 14:39:12 +0100

    style: prettier

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,33 @@
-const mongoose = require("mongoose");
-const { isEmail } = require("validator");
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
-    email: { 
-        type: String, 
-        required: [true, 'Please enter an email'],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail,'Please enter a valid email'] 
-    },
-    password: { 
-        type: String, 
-        required: [true, 'Please enter a password'],
-        minlength: [6, 'Minimum password length is 6 characters'] 
+    email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
 },
+    password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+}
 });
 
-const User = mongoose.model("user", userSchema);
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+    console.log('new user was created & saved', doc);
+    next();
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+    console.log('user about to be created & saved', this);
+    next();
+});
+
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
